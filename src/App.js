ca27e4a5ee856bf03c9f6b5788f2837539c6ff4d
ibrahimAshraff <ibrahim.ashraff@microbridge.com.ng>
2021-10-08 10:53:43 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './assets/igori-gaming-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="igor-logo" />
+        <p style={{ fontWeight: "bold" }}>
+          Igori Gaming Coming Soon
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
